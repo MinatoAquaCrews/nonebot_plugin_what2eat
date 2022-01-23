65a80da7bc8e708e943a602616cf5c7d1982a3a9
KafCoppelia <ubuntu@localhost.localdomain>
2022-01-23 20:41:25 +0800

    更新v0.1.1🥳

--- nonebot_plugin_what2eat/__init__.py
+++ nonebot_plugin_what2eat/__init__.py
@@ -1,7 +1,7 @@
 from nonebot import on_command, on_regex
-from nonebot.adapters.cqhttp.permission import GROUP
-from nonebot.adapters.cqhttp import Bot, GroupMessageEvent, MessageEvent
-import nonebot
+from nonebot.permission import SUPERUSER
+from nonebot.adapters.cqhttp.permission import GROUP, GROUP_ADMIN, GROUP_OWNER
+from nonebot.adapters.cqhttp import Bot, GroupMessageEvent
 from nonebot.typing import T_State
 from nonebot.log import logger
 from .utils import eating_manager
@@ -9,53 +9,81 @@ from .utils import eating_manager
 from nonebot import require
 scheduler = require("nonebot_plugin_apscheduler").scheduler
 
-what2eat = on_regex(r"^(今天|[早中午晚][上饭餐午]|夜宵|今晚)吃(什么|啥|点啥)", priority=15, block=True)
-add_food = on_command("添加", permission=GROUP, priority=15, block=True)
-remove_food = on_command("移除", permission=GROUP, priority=15, block=True)
+what2eat = on_regex(r"^(今天|[早中午晚][上饭餐午]|早上|夜宵|今晚)吃(什么|啥|点啥)", permission=GROUP, priority=15, block=True)
+add_group = on_command("添加", permission=SUPERUSER | GROUP_ADMIN | GROUP_OWNER, priority=15, block=True)
+remove_food = on_command("移除", permission=SUPERUSER | GROUP_ADMIN | GROUP_OWNER, priority=15, block=True)
+add_basic = on_command("加菜", permission=SUPERUSER, priority=15, block=True)
+show = on_command("查看菜单", aliases={"群菜单"}, permission=GROUP, priority=15, block=True)
 
 @what2eat.handle()
 async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
-    user_id = str(event.user_id)
-    result = eating_manager.get2eat(user_id, event)
-    
-    await what2eat.finish(result)
+    msg = eating_manager.get2eat(event)
+    await what2eat.finish(msg)
 
-@add_food.handle()
+@add_group.handle()
 async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
     args = str(event.get_plaintext()).strip().split()
     if not args:
-        await add_food.finish("还没输入你要添加的菜品呢~")
+        await add_group.finish("还没输入你要添加的菜品呢~")
     elif args and len(args) == 1:
         new_food = args[0]
     else:
-        await add_food.finish("添加菜品参数错误~")
+        await add_group.finish("添加菜品参数错误~")
     
     user_id = str(event.user_id)
     logger.info(f"User {user_id} 添加了 {new_food} 至菜单")
-    await add_food.finish(eating_manager.add_food(new_food))
+    msg = eating_manager.add_group_food(new_food, event)
+
+    await add_group.finish(msg)
+
+@add_basic.handle()
+async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
+    args = str(event.get_plaintext()).strip().split()
+    if not args:
+        await add_basic.finish("还没输入你要添加的菜品呢~")
+    elif args and len(args) == 1:
+        new_food = args[0]
+    else:
+        await add_basic.finish("添加菜品参数错误~")
+    
+    user_id = str(event.user_id)
+    logger.info(f"Superuser {user_id} 添加了 {new_food} 至基础菜单")
+    msg = eating_manager.add_basic_food(new_food)
+
+    await add_basic.finish(msg)
 
 @remove_food.handle()
 async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
     args = str(event.get_plaintext()).strip().split()
     if not args:
-        await add_food.finish("还没输入你要移除的菜品呢~")
+        await remove_food.finish("还没输入你要移除的菜品呢~")
     elif args and len(args) == 1:
         food_to_remove = args[0]
     else:
-        await add_food.finish("移除菜品参数错误~")
+        await remove_food.finish("移除菜品参数错误~")
     
     user_id = str(event.user_id)
     logger.info(f"User {user_id} 从菜单移除了 {food_to_remove}")
-    await add_food.finish(eating_manager.remove_food(food_to_remove))
+    msg = eating_manager.remove_food(food_to_remove, event)
+
+    await remove_food.finish(msg)
+
+@show.handle()
+async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
+    '''
+        show_basic: 显示基础菜单，避免菜单过长导致刷屏
+    '''
+    msg = eating_manager.show_menu(event, show_basic=False)
+    await show.finish(msg)
 
 
-# 重置三餐吃什么次数
+# 重置吃什么次数，包括夜宵
 @scheduler.scheduled_job(
     "cron",
-    hour="6,11,17", # 每日6、11、17时执行
+    hour="6,11,17,22",
     minute=0,
 )
 
 async def _():
-    eating_manager.reset_eating_times()
+    eating_manager.reset_eating()
     logger.info("今天吃什么次数已刷新")
--- nonebot_plugin_what2eat/utils.py
+++ nonebot_plugin_what2eat/utils.py
@@ -1,103 +1,218 @@
-from nonebot.adapters.cqhttp import GroupMessageEvent
+from nonebot.adapters.cqhttp import GroupMessageEvent, MessageSegment
 import random
 from pathlib import Path
-from typing import Optional, Union, List, Dict
+from typing import Optional
 import nonebot
-import os
+from enum import Enum
 
 try:
     import ujson as json
 except ModuleNotFoundError:
     import json
 
+SUPERUSERS = nonebot.get_driver().config.superusers
 _WHAT2EAT_PATH = nonebot.get_driver().config.what2eat_path
-DEFAULT_PATH = os.path.join(os.path.dirname(__file__), "resource")
+DEFAULT_PATH = "./data/what2eat"
 WHAT2EAT_PATH = DEFAULT_PATH if not _WHAT2EAT_PATH else _WHAT2EAT_PATH
-
 _EATING_LIMIT = nonebot.get_driver().config.eating_limit
-EATING_LIMIT = 5 if not _EATING_LIMIT else _EATING_LIMIT
+EATING_LIMIT = 6 if not _EATING_LIMIT else _EATING_LIMIT
+
+'''
+    Reserved for next version
+'''
+class Meals(Enum):
+    BREAKFAST   = "_breakfast"
+    LUNCH       = "_lunch"
+    DINNER      = "_dinner"
+    SNACK       = "_midnight_snack"
 
 class EatingManager:
 
-    def __init__(self, file: Optional[Path]):
+    def __init__(self, file_path: Optional[Path]):
         self._data = {}
-        if not file:
+        if not file_path:
             file = Path(WHAT2EAT_PATH) / "data.json"
+        else:
+            file = file_path / "data.json"
         
         self.file = file
         if file.exists():
             with open(file, "r", encoding="utf-8") as f:
                 self._data = json.load(f)
+
+        self._init_json()
+
+    def _init_json(self) -> None:
+        # 建议["basic_food"]初始非空
+        if "basic_food" not in self._data.keys():
+            self._data["basic_food"] = []
+        if "group_food" not in self._data.keys():
+            self._data["group_food"] = {}
+        if "eating" not in self._data.keys():
+            self._data["eating"] = {}
     
-    def _init_data(self, event: GroupMessageEvent):
+    def _init_data(self, event: GroupMessageEvent) -> None:
         '''
             初始化用户信息
         '''
         user_id = str(event.user_id)
-        if "eating_times" not in self._data.keys():
-            self._data["eating_times"] = {}
+        group_id = str(event.group_id)
+        
+        if group_id not in self._data["group_food"].keys():
+            self._data["group_food"][group_id] = []
+        if group_id not in self._data["eating"].keys():
+            self._data["eating"][group_id] = {}
+        if user_id not in self._data["eating"][group_id].keys():
+            self._data["eating"][group_id][user_id] = 0
 
-        if user_id not in self._data["eating_times"].keys():
-            self._data["eating_times"][user_id] = 0
+    def get2eat(self, event: GroupMessageEvent) -> str:
+        '''
+            今天吃什么
+        '''
+        user_id = str(event.user_id)
+        group_id = str(event.group_id)
 
-    def get2eat(self, user_id: str, event: GroupMessageEvent) -> str:
-        '''
-            Bot给出建议
-        '''
         self._init_data(event)
-        if not self.eat_limit_check(user_id):
+        if not self.eating_check(event):
             return random.choice(
                 [
                     "你今天已经吃得够多了！",
                     "吃这么多的吗？",
-                    "害搁这吃呢？不工作的吗？"
+                    "害搁这吃呢？不工作的吗？",
+                    "再吃肚子就要爆炸咯~"
                 ]
             )
         else:
-            result = "建议" + random.choice(self._data["food"])
-            self._data["eating_times"][user_id] = self._data["eating_times"][user_id] + 1
+            # 菜单全为空，建议避免["basic_food"]为空
+            if len(self._data["basic_food"]) == 0 and len(self._data["group_food"][group_id]) == 0:
+                return "还没有菜单呢，就先饿着肚子吧，请[添加 菜名]🤤"
+            
+            food_list = self._data["basic_food"].copy()
+            if len(self._data["group_food"][group_id]) > 0:
+                food_list.extend(self._data["group_food"][group_id])
+
+            msg = "建议" + random.choice(food_list)
+            self._data["eating"][group_id][user_id] += 1
             self.save()
 
-            return result
-
-    def eat_limit_check(self, user_id: str) -> bool:
-        '''
-            检查是否达到询问上限
-        '''
-        return False if self._data["eating_times"][user_id] >= EATING_LIMIT else True
-
-    def add_food(self, new_food: str) -> str:
-        for food in self._data["food"]:
-            if food == new_food:
-                return f"{new_food}已经在菜单中了~"
-        
-        self._data["food"].append(new_food)
-        self.save()
-        return f"{new_food}已加入菜单~"
+            return msg
     
-    def remove_food(self, food_to_remove: str) -> str:
-        for food in self._data["food"]:
-            if food == food_to_remove:
-                self._data["food"].remove(food_to_remove)
-                self.save()
-                return f"{food_to_remove}已从菜单中删除~"
-        
-        return f"{food_to_remove}不在菜单中哦~"
+    '''
+        检查菜品是否存在
+        1:  存在于基础菜单
+        2:  存在于群菜单
+        0:  不存在
+    '''
+    def food_exists(self, _food_: str) -> int:
+        for food in self._data["basic_food"]:
+            if food == _food_:
+                return 1
 
-    def reset_eating_times(self):
+        for group_id in self._data["group_food"]:
+            for food in self._data["group_food"][group_id]:
+                if food == _food_:
+                    return 2
+        
+        return 0
+
+    '''
+        检查是否吃饱
+    '''
+    def eating_check(self, event: GroupMessageEvent) -> bool:
+        user_id = str(event.user_id)
+        group_id = str(event.group_id)
+        return False if self._data["eating"][group_id][user_id] >= EATING_LIMIT else True
+
+    '''
+        添加至群菜单中 GROUP_ADMIN | GROUP_OWNER 权限
+    '''
+    def add_group_food(self, new_food: str, event: GroupMessageEvent) -> str:
+        group_id = str(event.group_id)
+
+        status = self.food_exists(new_food)
+        if status == 1:
+            return f"{new_food} 已在基础菜单中~"
+        elif status == 2:
+            return f"{new_food} 已在群特色菜单中~"
+
+        self._data["group_food"][group_id].append(new_food)
+        self.save()
+        return f"{new_food} 已加入群特色菜单~"
+
+    '''
+        添加至基础菜单 SUPERUSER 权限
+    '''
+    def add_basic_food(self, new_food: str) -> str:
+        status = self.food_exists(new_food)
+        if status == 1:
+            return f"{new_food} 已在基础菜单中~"
+        elif status == 2:
+            return f"{new_food} 已在群特色菜单中~"
+
+        self._data["basic_food"].append(new_food)
+        self.save()
+        return f"{new_food} 已加入基础菜单~"
+
+    '''
+        从基础菜单移除 SUPERUSER 权限
+        从群菜单中移除 GROUP_ADMIN | GROUP_OWNER 权限
+    '''
+    def remove_food(self, food_to_remove: str, event: GroupMessageEvent) -> str:
+        user_id = str(event.user_id)
+        group_id = str(event.group_id)
+        
+        status = self.food_exists(food_to_remove)
+        if not status:
+            return f"{food_to_remove} 不在菜单中哦~"
+
+        # 在群菜单
+        if status == 2:
+            self._data["group_food"][group_id].remove(food_to_remove)
+            self.save()
+            return f"{food_to_remove} 已从群菜单中删除~"
+        # 在基础菜单
+        else:
+            if user_id not in SUPERUSERS:
+                return f"{food_to_remove} 在基础菜单中，非超管不可操作哦~"
+            else:
+                self._data["basic_food"].remove(food_to_remove)
+                self.save()
+                return f"{food_to_remove} 已从基础菜单中删除~"    
+
+    def reset_eating(self) -> None:
         '''
-            重置三餐eating_limit
+            重置三餐eating times
         '''
-        for user_id in self._data["eating_times"].keys():
-            self._data["eating_times"][user_id] = 0
+        for group_id in self._data["eating"].keys():
+            for user_id in self._data["eating"][group_id].keys():
+                self._data["eating"][group_id][user_id] = 0
         
         self.save()
 
-    def save(self):
+    def save(self) -> None:
         '''
             保存数据
         '''
         with open(self.file, 'w', encoding='utf-8') as f:
             json.dump(self._data, f, ensure_ascii=False, indent=4)
 
-eating_manager = EatingManager(Path(WHAT2EAT_PATH) / "data.json")
+    def show_menu(self, event: GroupMessageEvent, show_basic: bool) -> str:
+        group_id = str(event.group_id)
+        msg = []
+        
+        if len(self._data["group_food"][group_id]) > 0:
+            msg += MessageSegment.text("---群特色菜单---\n")
+            for food in self._data["group_food"][group_id]:
+                msg += MessageSegment.text(f"{food}\n")
+
+        if len(self._data["basic_food"]) > 0 and show_basic:
+            msg += MessageSegment.text("---基础菜单---\n")
+            for food in self._data["basic_food"]:
+                msg += MessageSegment.text(f"{food}\n")
+        
+        if show_basic:
+            return msg if len(msg) > 0 else "还没有菜单呢，请[添加 菜名]🤤"
+        else:
+            return msg if len(msg) > 0 else "没有群特色菜单，请[添加 菜名]🤤"
+
+eating_manager = EatingManager(Path(WHAT2EAT_PATH))
